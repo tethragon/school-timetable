bf344c0eaf1282af58d3693f3b8a4c2a21cc8a70
tethragon <39238674+tethragon@users.noreply.github.com>
2026-09-03 02:04:36 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/school-timetable/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
